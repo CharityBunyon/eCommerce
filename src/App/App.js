b074015fd987039b3f316a9c1c99f0e424033dcc
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-05-12 21:53:37 -0500

    added setup files

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -18,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <button className='btn btn-danger'>LET THE FUN BEGIN</button>
       </header>
     </div>
   );
